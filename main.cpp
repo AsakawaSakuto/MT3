342281f2b8dbbfdcc36de4cde285759295b5780e
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2025-05-21 13:05:03 +0900

    課題内容完了

--- main.cpp
+++ main.cpp
@@ -4,11 +4,75 @@
 #include"imgui.h"
 #include"Matrix4x4Function.h"
 #include"DrawObject.h"
+#include <algorithm>
 
 const char kWindowTitle[] = "LE2B_02_アサカワ_サクト";
 
 int kWindowWidth = 1280;
 int kWindowHeight = 720;
+
+//線分
+struct Segment {
+	Vector3 origin;
+	Vector3 diff;
+};
+
+//加算
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 v3;
+	v3 = { v1.x + v2.x,v1.y + v2.y ,v1.z + v2.z };
+	return v3;
+};
+
+//減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 v3;
+	v3 = { v1.x - v2.x,v1.y - v2.y ,v1.z - v2.z };
+	return v3;
+};
+
+//スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v) {
+	Vector3 v3;
+	v3 = { scalar * v.x,scalar * v.y,scalar * v.z };
+	return v3;
+};
+
+//内積
+float Dot(const Vector3& v1, const Vector3& v2) {
+	float r;
+	r = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	return r;
+};
+
+//長さ（ノルム）
+float Length(const Vector3& v) {
+	float length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	return length;
+};
+
+//正規化
+Vector3 Normalize(const Vector3& v) {
+	float length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	Vector3 r;
+	r = { v.x / length,v.y / length ,v.z / length };
+	return r;
+};
+
+//正射影ベクトル
+Vector3 Project(const Vector3& v1, const Vector3& v2) {
+	float v2SqLength = Dot(v2, v2);
+	float dot = Dot(v1, v2);
+	return Multiply(dot / v2SqLength, v2);
+};
+
+//最近接点
+Vector3 ClosestPoint(const Vector3& point, const Segment& segment) {
+	Vector3 v = Subtract(point, segment.origin);
+	float t = Dot(v, segment.diff) / Dot(segment.diff, segment.diff);
+	t = std::clamp(t, 0.0f, 1.0f);
+	return Add(segment.origin, Multiply(t, segment.diff));
+};
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -19,6 +83,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
+
+	Segment segment{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
+	Vector3 point{ -1.5f,0.6f,0.6f };
+
+	Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
+	Vector3 closestPoint = ClosestPoint(point, segment);
+
+	Sphere pointSphere{ point,0.01f };
+	Sphere closestPointSphere{ closestPoint,0.01f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -33,6 +109,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = InverseMatrix(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		
+		Matrix4x4 worldViewProjectionMatrix = MultiplyMatrix(viewMatrix, projectionMatrix);
+
+		Matrix4x4 viewportMatrix = MakeViewPortMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		Vector3 start = Transform(Transform(segment.origin, worldViewProjectionMatrix), viewportMatrix);
+		Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), worldViewProjectionMatrix), viewportMatrix);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -40,6 +127,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
+
+		DrawSphere(pointSphere, worldViewProjectionMatrix, viewportMatrix, RED);
+		DrawSphere(closestPointSphere, worldViewProjectionMatrix, viewportMatrix, BLACK);
+
+		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), WHITE);
+
+		ImGui::Begin("Window");
+		ImGui::InputFloat3("Point", &point.x, "%.3f");
+		ImGui::InputFloat3("Segment origin", &segment.origin.x, "%.3f");
+		ImGui::InputFloat3("Segment diff", &segment.diff.x, "%.3f");
+		ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+		ImGui::End();
 
 		///
 		/// ↑描画処理ここまで
