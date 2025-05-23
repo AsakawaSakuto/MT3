7ec38b9c93cfd3fccd268c0be482164ef181ca08
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2025-05-23 11:34:15 +0900

    課題内容完了

--- main.cpp
+++ main.cpp
@@ -12,6 +12,32 @@ const char kWindowTitle[] = "LE2B_02_アサカワ_サクト";
 int kWindowWidth = 1280;
 int kWindowHeight = 720;
 
+// 衝突判定
+bool IsCollision(const Segment& segment, const Plane& plane)
+{
+	// 垂直判定を行うために、法線と線の内積を求める
+	float dot = Dot(plane.normal, segment.diff);
+
+	// 垂直 = 平行であるので、衝突していない
+	if (dot == 0.0f)
+	{
+		return false;
+	}
+
+	// t を求める
+	float t = (plane.distance - Dot(segment.origin, plane.normal)) / dot;
+
+	// t の値と線の種類によって衝突しているか判断
+	if (t >= 0 && t <= 1)
+	{
+		return true;
+	} 
+	else
+	{
+		return false;
+	}
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -21,6 +47,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	// カメラ
+	Vector3 cameraScale{ 1.0f,1.0f,1.0f };
+	Vector3 cameraTranslate{ 0.0f,2.5f,-10.f };// カメラの位置
+	Vector3 cameraRotate{ 0.2f,0.0f,0.0f };// カメラの角度
+
+	// 平面
+	Plane plane = {
+		{0.0f,1.0f,0.0f},
+		1.0f,
+	};
+
+	// 線
+	Segment segment{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
+	Vector3 point{ -1.5f,0.6f,0.6f };
+
+	// 色
+	int color = WHITE;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -35,6 +79,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 worldMatrix = MakeAffineMatrix(cameraScale, cameraRotate, cameraTranslate);
+
+		Matrix4x4 viewMatrix = InverseMatrix(worldMatrix);
+
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 1.0f, 0.0f);
+
+		Matrix4x4 worldViewProjectionMatrix = MultiplyMatrix(viewMatrix, projectionMatrix);
+
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		// 正射影ベクトル
+		Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
+
+		// 最近接点
+		Vector3 closestPoint = ClosestPoint(point, segment);
+
+		// 線の始点終点
+		Vector3 start = Transform(Transform(segment.origin, worldViewProjectionMatrix), viewportMatrix);
+		Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), worldViewProjectionMatrix), viewportMatrix);
+		
+		// 当たり判定
+		if (IsCollision(segment, plane))
+		{
+			color = RED;
+		}
+		else 
+		{
+			color = WHITE;
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -43,8 +117,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		// グリッド
+		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
+
+		// 平面
+		DrawPlane(plane, worldViewProjectionMatrix, viewportMatrix, WHITE);
+
+		// 線
+		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), color);
+
 		ImGui::Begin("Window");
 		
+		ImGui::DragFloat3("cameraScale", &cameraScale.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+
+		ImGui::DragFloat3("Segment.origin", &segment.origin.x, 0.01f);
+		ImGui::DragFloat3("Segment.diff", &segment.diff.x, 0.01f);
+
+		ImGui::DragFloat3("Plane.Normal", &plane.normal.x, 0.01f);
+		ImGui::DragFloat("Plane.Distence", &plane.distance, 0.01f);
+		// 法線を編集したら Normalize をかける。平面法線が単位ベクトル前提で組まれているから。
+		plane.normal = Normalize(plane.normal);
+
 		ImGui::End();
 
 		///
